6adf94ca7d91117117b45025aa4fc54d0987fc97
Wilson Martins <martins.wilson17@gmail.com>
2020-06-11 00:18:05 -0300

    Adding DDL

--- src/main/java/com/mc855/tracker/app/Application.java
+++ src/main/java/com/mc855/tracker/app/Application.java
@@ -4,18 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
---
+++ src/main/resources/db/ddl.sql
@@ -0,0 +1,96 @@
+create table person
+(
+    person_id int auto_increment,
+    first_name varchar(40) null,
+    middle_name varchar(40) null,
+    last_name varchar(40) null,
+    email_addr varchar(80) null,
+    phone_num varchar(40) null,
+    birth_day int null,
+    birth_month int null,
+    birth_year int null,
+    create_user varchar(40) null,
+    create_dt datetime null,
+    update_user varchar(40) null,
+    update_dt datetime null,
+    constraint person_pk
+        primary key (person_id)
+);
+
+create table status_history
+(
+    status_history_id int auto_increment,
+    disease_id int null,
+    status_dt datetime null,
+    value varchar(10) null,
+    create_user varchar(40) null,
+    create_dt datetime null,
+    update_user varchar(40) null,
+    update_dt datetime null,
+    constraint status_history_pk
+        primary key (status_history_id)
+);
+
+create table disease
+(
+    disease_id int auto_increment,
+    name varchar(40) null,
+    incubation_period int null,
+    total_period int null,
+    create_user varchar(40) null,
+    create_dt datetime null,
+    update_user varchar(40) null,
+    update_dt datetime null,
+    constraint disease_pk
+        primary key (disease_id)
+);
+
+create table person_contact
+(
+    person_contact_id int auto_increment,
+    person_1_id       int         null,
+    person_2_id       int         null,
+    date_start        int         null,
+    date_end          int         null,
+    proximity         int         null,
+    create_user       varchar(40) null,
+    create_dt         datetime    null,
+    update_user       varchar(40) null,
+    update_dt         datetime    null,
+    constraint person_contact_pk
+        primary key (person_contact_id)
+);
+
+create table address
+(
+    address_id  int auto_increment,
+    person_id   int          null,
+    addr_1      varchar(100) null,
+    addr_2      varchar(100) null,
+    city        varchar(100) null,
+    state       varchar(100) null,
+    country_cd  varchar(10)  null,
+    zip_cd      varchar(20)  null,
+    type_cd     varchar(10)  null,
+    notes       varchar(100) null,
+    create_user varchar(40)  null,
+    create_dt   datetime     null,
+    update_user varchar(40)  null,
+    update_dt   datetime     null,
+    constraint address_pk
+        primary key (address_id)
+);
+
+create table person_location_config
+(
+    person_location_config_id int auto_increment,
+    disease_id                int         null,
+    threshold_distance        int         null,
+    threshold_duration        int         null,
+    create_user               varchar(40) null,
+    create_dt                 datetime    null,
+    update_user               varchar(40) null,
+    update_dt                 datetime    null,
+    constraint person_location_config_pk
+        primary key (person_location_config_id)
+);
