cd0914e569dfb822c02e93b4f84e94b70e641e6d
Caio Cesar <caio.cdcs@gmail.com>
2020-06-24 21:00:15 -0300

    adding endpoint to add person

--- src/main/java/com/mc855/tracker/web/rest/controller/PersonController.java
+++ src/main/java/com/mc855/tracker/web/rest/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.mc855.tracker.web.rest.controller;
 import com.mc855.tracker.service.PersonBO;
 import com.mc855.tracker.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +17,28 @@ public class PersonController {
     private PersonBO personBO;
 
     @GetMapping("")
-    public List<Person> getPersons() {
-        return personBO.getPersons();
+    public ResponseEntity<List<Person>> getPersons() {
+        return new ResponseEntity(personBO.getPersons(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable Long id) {
-        return personBO.getPerson(id);
+    public ResponseEntity<Person> getPerson(@PathVariable Long id) {
+        return new ResponseEntity(personBO.getPerson(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePerson(@PathVariable Long id) {
+    public ResponseEntity deletePerson(@PathVariable Long id) {
         personBO.deletePerson(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("")
-    public Person updatePerson(@RequestBody Person person) {
-        return personBO.updatePerson(person);
+    public ResponseEntity<Person> updatePerson(@RequestBody Person person) {
+        return new ResponseEntity(personBO.updatePerson(person), HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+        return new ResponseEntity(personBO.updatePerson(person), HttpStatus.CREATED);
     }
 }
