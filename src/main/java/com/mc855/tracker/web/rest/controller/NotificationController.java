f5656fdb766d357e3e1b119ec7f76c15e452e5e7
Wilson Martins <martins.wilson17@gmail.com>
2020-08-21 14:08:12 -0300

    Fix Notification path

--- src/main/java/com/mc855/tracker/web/rest/controller/NotificationController.java
+++ src/main/java/com/mc855/tracker/web/rest/controller/NotificationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/location")
+@RequestMapping("/notification")
 public class NotificationController {
 
     @Autowired
