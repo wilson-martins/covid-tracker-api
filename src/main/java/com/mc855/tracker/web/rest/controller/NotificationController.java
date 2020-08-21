1047dee6f72702528ed25e8801800ba3552052fe
Wilson Martins <martins.wilson17@gmail.com>
2020-08-21 14:10:01 -0300

    Notification test endpoint

--- src/main/java/com/mc855/tracker/web/rest/controller/NotificationController.java
+++ src/main/java/com/mc855/tracker/web/rest/controller/NotificationController.java
@@ -1,15 +1,17 @@
 package com.mc855.tracker.web.rest.controller;
 
+import com.mc855.tracker.repository.PersonRepository;
 import com.mc855.tracker.service.NotificationService;
-import com.mc855.tracker.service.dto.LocationDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @Slf4j
 @RestController
@@ -17,14 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     @Autowired
-    public NotificationService notificationService;
-    @PutMapping("")
-    public ResponseEntity<String> register(@RequestBody LocationDto locationDto) {
+    private NotificationService notificationService;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @PutMapping("/{personId}")
+    public ResponseEntity<String> register(@PathVariable("personId") Long personId) {
         try {
-//            this.locationBO.register(locationDto);
-            log.info("Successfully executed /location/register");
+            this.notificationService.sendNotifications(Arrays.asList(this.personRepository.findById(personId).orElse(null)));
+            log.info("Successfully executed /notification/");
         } catch (Exception e) {
-            log.error("Error executing /location/register", e);
+            log.error("Error executing /notification/", e);
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>("OK", HttpStatus.OK);
