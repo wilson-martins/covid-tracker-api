347536e17dc1c6dc28aec81b6ec3686251875545
Wilson Martins <martins.wilson17@gmail.com>
2020-08-21 16:07:55 -0300

    Sending notifications

--- src/main/java/com/mc855/tracker/jobs/ScheduleTasks.java
+++ src/main/java/com/mc855/tracker/jobs/ScheduleTasks.java
@@ -3,6 +3,7 @@ package com.mc855.tracker.jobs;
 import com.mc855.tracker.domain.Person;
 import com.mc855.tracker.domain.reference.HealthState;
 import com.mc855.tracker.service.LocationBO;
+import com.mc855.tracker.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,10 +19,14 @@ public class ScheduleTasks {
     @Autowired
     private LocationBO locationBO;
 
-    @Scheduled(fixedDelay = 240000)
+    @Autowired
+    private NotificationService notificationService;
+
+    @Scheduled(fixedDelay = 180000)
     public void checkRisks() {
         HealthState[] healthStates = {HealthState.POSITIVE, HealthState.CURED};
         Collection<Person> persons = this.locationBO.checkRisk(null, Arrays.asList(healthStates));
         log.info("Found [{}] persons to notify", persons.size());
+        this.notificationService.sendNotifications(persons);
     }
 }
--- src/main/java/com/mc855/tracker/service/PersonBO.java
+++ src/main/java/com/mc855/tracker/service/PersonBO.java
@@ -1,7 +1,10 @@
 package com.mc855.tracker.service;
 
 import com.mc855.tracker.domain.Person;
+import com.mc855.tracker.domain.StatusHistory;
+import com.mc855.tracker.domain.reference.HealthState;
 import com.mc855.tracker.repository.PersonRepository;
+import com.mc855.tracker.repository.StatusHistoryRepository;
 import com.mc855.tracker.service.dto.UpdateGoogleIdTokenDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,7 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +21,9 @@ public class PersonBO {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @Autowired
+    private StatusHistoryRepository statusHistoryRepository;
 
     public List<Person> getPersons() {
         return personRepository.findAll();
@@ -40,7 +47,17 @@ public class PersonBO {
     }
 
     public Person updatePerson(Person person) {
-        return personRepository.save(person);
+        Person dbPerson = personRepository.save(person);
+
+        StatusHistory statusHistory = new StatusHistory();
+        statusHistory.setDiseaseId(1L);
+        statusHistory.setPersonId(dbPerson.getPersonId());
+        statusHistory.setStatusDt(new Date());
+        statusHistory.setValue(HealthState.NEGATIVE);
+
+        this.statusHistoryRepository.save(statusHistory);
+
+        return dbPerson;
     }
 
     public Boolean setGoogleId(UpdateGoogleIdTokenDto updateGoogleIdTokenDto) {
